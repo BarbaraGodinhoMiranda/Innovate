5ca969da8026f06de665bcf926144389f362c6f4
BARBARA GODINHO MIRANDA <barbaragodinho19@gmail.com>
2024-08-09 15:21:52 +0200

    Update MailConfig.java

--- demo/MailConfig.java
+++ demo/MailConfig.java
@@ -17,8 +17,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("barbaragodinho19@gmail.com");
-        mailSender.setPassword("owpj usdg tang yxgk");
+        mailSender.setUsername("xxxxxxxxxxxxxx@gmail.com");
+        mailSender.setPassword("xxxxxxxxxxxxxxx");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
